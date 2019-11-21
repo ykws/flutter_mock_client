1af049b9d0e4b72ef05e9a4886dbf719e5904211
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-21 12:09:13 +0900

    Write a test for each condition

--- test/post_test.dart
+++ test/post_test.dart
@@ -1,8 +1,29 @@
+import 'package:test/test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:flutter_mock_client/post.dart';
 
 class MockClient extends Mock implements http.Client {}
 
 main() {
-  // Tests go here
+  group('fetchPost', () {
+    test('returns a Post if the http call completes successfully', () async {
+      final client = MockClient();
+
+      when(client.get('https://jsonplaceholder.typicode.com/posts/1'))
+        .thenAnswer((_) async => http.Response('{"title": "Test"}', 200));
+
+      expect(await fetchPost(client), const TypeMatcher<Post>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      when(client.get('https://jsonplaceholder.typicode.com/posts/1'))
+        .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchPost(client), throwsException);
+    });
+  });
 }
